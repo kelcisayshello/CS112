d7ed136a04a6c23d7cd83efba77243f10a51f52d
k3nsah <kelcimensah@outlook.com>
2022-03-17 21:09:23 -0400

    fixed duplicating issues

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ for (i = 0; i < closebtns.length; i++) {
 
 function displayContent(event, problemset) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("menulinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
