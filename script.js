8c9c9736f51a2e84376286000c9fcaa7009e6185
k3nsah <kelcimensah@outlook.com>
2022-03-17 20:01:09 -0400

    slight organizational changes to files

---
+++ script.js
@@ -0,0 +1,28 @@
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+    closebtns[i].addEventListener("click", function() {
+        this.style.display = 'none';
+    });
+}
+
+function displayContent(event, problemset) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(problemset).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+$(document).ready(function(){
+    $('#ps-bigo').load("problemsets/bigo.html");
+    $('#ps2').load("problemsets/bigo.html");
+    $('#ps3').load("problemsets/bigo.html");
+});
